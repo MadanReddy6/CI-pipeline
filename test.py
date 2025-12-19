611f1f8c01423af2e0bf5948d84ddac34119e116
Madan Reddy <kmmr656@gmail.com>
2025-12-19 18:35:46 +0000

    test file added

--- test.py
+++
@@ -1,3 +0,0 @@
-print("test is starting")
-sleep(5)
-print("test is ended")
---
+++ test_sample.py
@@ -0,0 +1,7 @@
+import time
+
+def test_example():
+    print("test is starting")
+    time.sleep(1)
+    print("test is ended")
+    assert True
